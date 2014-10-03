ef343d1aae7db8e4917b8ac946c911bf23a42ffc
giftstarter-stuart <stuart@giftstarter.co>
2014-10-02 20:58:06 -0700

    First (second) commit

---
+++ ng-ab.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Stuart on 10/2/14.
+ */
+
+angular.module('ngAB', [])
+    .value('ABSpec', {})
+    .factory('ABInterceptor', [
+        'ABSpec',
+        function(ABSpec) {
+            return {
+                response: function(response) {
+                    var spec = ABSpec[response.config.url];
+                    if (!spec) {
+                        // No test spec for this path
+                        return response;
+                    }
+
+                    // Create obj of tests and chosen permutations
+                    var permutation = Object.keys(spec).reduce(function(obj, k) {
+                        obj[k] = spec[k].perm;
+                        return obj;
+                    }, {});
+
+
+                    function applyChange(data, change) {
+                        if (!change.find) {
+                            return data + change.replace;
+                        } else {
+                            return data.replace(new RegExp(change.find,
+                                change.flags), change.replace);
+                        }
+                    }
+
+                    response.data = Object.keys(spec).reduce(function(data, key) {
+                        return spec[key].changes.reduce(applyChange, data);
+                    }, response.data);
+
+                    return response;
+                }
+            }
+        }
+    ]).config([
+        '$httpProvider',
+        function($httpProvider) {
+            $httpProvider.interceptors.push('ABInterceptor');
+        }
+    ]);
